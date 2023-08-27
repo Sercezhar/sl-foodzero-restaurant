277e2611be211234442251e74917feae9849d348
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-08-27 11:02:53 +0300

    update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,16 @@
+import { datepicker } from './modules/datepicker';
+import { dropdown } from './modules/dropdown';
 import { navMenu } from './modules/navMenu';
+import { timepicker } from './modules/timepicker';
 
 navMenu();
+datepicker();
+dropdown(
+  '.dropdown__input',
+  '.dropdown-options',
+  '.dropdown-options__item',
+  '.dropdown__icon',
+  'dropdown__icon--rotate',
+  'dropdown-options__item--active'
+);
+timepicker();
