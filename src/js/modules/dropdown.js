a6e8ed8bc90cb94720e01f4268935fb411003171
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-08-27 15:21:50 +0300

    update dropdown.js

--- src/js/modules/dropdown.js
+++ src/js/modules/dropdown.js
@@ -2,9 +2,7 @@ export function dropdown(
   dropdownSelector,
   optionsSelector,
   itemSelector,
-  iconSelector,
-  iconActive,
-  itemActive
+  iconSelector
 ) {
   const dropdownButton = document.querySelector(dropdownSelector);
   const options = document.querySelector(optionsSelector);
@@ -16,24 +14,27 @@ export function dropdown(
     item.addEventListener('click', () => handleOptionClick(item));
   });
 
+  const iconActiveClass = `${iconSelector.slice(1)}--rotate`;
+  const itemActiveClass = `${itemSelector.slice(1)}--active`;
+
   function hideOptions() {
     options.classList.add('visually-hidden');
-    icon.classList.remove(iconActive);
+    icon.classList.remove(iconActiveClass);
   }
 
   function handleOptionClick(item) {
     dropdownButton.value = item.dataset.option;
     hideOptions();
     optionsItem.forEach(option => {
-      option.classList.remove(itemActive);
+      option.classList.remove(itemActiveClass);
     });
-    item.classList.add(itemActive);
+    item.classList.add(itemActiveClass);
   }
 
   function handleOptionsVisibility(event) {
     if (event.target.matches(dropdownSelector)) {
       options.classList.toggle('visually-hidden');
-      icon.classList.toggle(iconActive);
+      icon.classList.toggle(iconActiveClass);
     }
 
     if (!event.target.matches(dropdownSelector)) {
